ecd91d1c2d93efde7787bc5e339eb3305a4593b7
HJP <HJP@qq.com>
2022-09-04 22:17:44 +0800

    first

---
+++ src/main/java/com/hjp/boot/MainApplication.java
@@ -0,0 +1,13 @@
+package com.hjp.boot;
+
+import com.hjp.boot.controller.HelloController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+
+    }
+}
