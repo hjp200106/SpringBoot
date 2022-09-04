7dc13dfacf6c07061b861518f0f71eb8e00566f0
HJP <HJP@qq.com>
2022-09-04 22:20:20 +0800

    first1.1

--- src/main/java/com/hjp/boot/controller/HelloController.java
+++ src/main/java/com/hjp/boot/controller/HelloController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String handle01(){
+        System.out.println("hello");
         return "hello,SpringBoot2!";
     }
+
 }
